281847b7b9c88f77915eb276b04d0f6e6508be49
Grimm <repisk@gmail.com>
2020-12-28 21:21:12 +0500

    search fix 1

---
+++ build/precache-manifest.b6631dede99b8e9c4b3ccd4865ff3d2d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afab87de9ec3df80c9b0107a5dcd1a5d",
+    "url": "/elements/index.html"
+  },
+  {
+    "revision": "94f59d5116e759e133e8",
+    "url": "/elements/static/css/main.febc3209.chunk.css"
+  },
+  {
+    "revision": "ecf09cc28dfcfd401683",
+    "url": "/elements/static/js/2.fdfc0965.chunk.js"
+  },
+  {
+    "revision": "8a137f586fcebb1a370343eb47b7677c",
+    "url": "/elements/static/js/2.fdfc0965.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94f59d5116e759e133e8",
+    "url": "/elements/static/js/main.185983c1.chunk.js"
+  },
+  {
+    "revision": "eff871fec7563501b264",
+    "url": "/elements/static/js/runtime-main.0517e7e1.js"
+  },
+  {
+    "revision": "0b15609c7dde5e151e1308550079448f",
+    "url": "/elements/static/media/slider.0b15609c.jpg"
+  },
+  {
+    "revision": "a35b62097279683658a6a314e6b5c4a2",
+    "url": "/elements/static/media/style.a35b6209.svg"
+  }
+]);
